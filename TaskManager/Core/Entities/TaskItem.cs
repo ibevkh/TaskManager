5e38623c727047177f29ec72cb2e80a75c1e2c1f
Iryna B <iryna.bevkh@gmail.com>
2025-10-22 12:06:43 +0300

    feat: add TaskItem model and WorkStatus enum

---
+++ TaskManager/Core/Entities/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿using TaskManager.Core.Enums;
+
+namespace TaskManager.Core.Entities;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public WorkStatus Status { get; set; }
+}
---
+++ TaskManager/Core/Enums/WorkStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Core.Enums;
+
+public enum WorkStatus
+{
+    Backlog,
+    InWork,
+    Testing,
+    Done
+}
